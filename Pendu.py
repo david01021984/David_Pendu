1520691840a7e45b6325be3d29bd8da0878f6934
MolenGeek <david.alvarez.lafuente@gmail.com>
2023-12-04 19:53:56 +0100

    avec les bons if

--- Pendu.py
+++ Pendu.py
@@ -27,67 +27,69 @@ def EnterLetter():
         else:
             print("Please enter just one letter please ;-) ")
 
-
-solution = wordPickUp()
-print(solution)
-nb_error = 0
-lettersBurned = []
-display = underscore(solution)
-print(f"Word to guess : {display} " )
-while True :
-    while '_' in display and nb_error < 5 :
-#        lettre = (input("Entrez une lettre : ")).upper()
-        lettre = EnterLetter()
-        if lettre not in lettersBurned:
-            lettersBurned += [lettre]
+def initGame():
+    solution = wordPickUp()
+#print(solution)
+    nb_error = 0
+    lettersBurned = []
+    display = underscore(solution)
+    print(f"Word to guess : {display} " )
+    while True :
+        while '_' in display and nb_error < 5 :
+#        letter = (input("Entrez une letter : ")).upper()
+            letter = EnterLetter()
+            if letter not in lettersBurned:
+                lettersBurned += [letter]
             
-        if lettre not in solution:
-            nb_error +=1           
+            if letter not in solution:
+                nb_error +=1           
         
-        display = underscore( solution , lettersBurned )
-        print(f"Word to guess : {display}        Errors : {nb_error}         Letters already used : {lettersBurned} \n")
-        if nb_error==0:
-            print(" ============ \n")
-        elif nb_error==1:
+            display = underscore( solution , lettersBurned )
+            print(f"Word to guess : {display}        Errors : {nb_error}         Letters already used : {lettersBurned} \n")
+            if nb_error==0 and '_' in display:
+                print(" ============ \n")
+            elif nb_error==1 and '_' in display:
+                print(" ==========Y= ")
+                print(" ||/       |  \n")
+            elif nb_error==2 and '_' in display:
+                print(" ==========Y= ")
+                print(" ||/       |  ")
+                print(" ||        0  \n")
+            elif nb_error==3 and '_' in display:
+                print(" ==========Y= ")
+                print(" ||/       |  ")
+                print(" ||        0  ")
+                print(" ||       /|\ \n")
+            elif nb_error==4 and '_' in display:
+                print(" ==========Y= ")
+                print(" ||/       |  ")
+                print(" ||        0  ")
+                print(" ||       /|\ ")
+                print(" ||       / \  \n")
+            elif nb_error==5 and '_' in display:  
+                print(" ==========Y= ")
+                print(" ||/       |  ")
+                print(" ||        0  ")
+                print(" ||       /|\ ")
+                print(" ||        |\  ")  
+                print(" ||            ")                 
+                print("/||\      (x)  ")
+                print("==============\n")
+                print("YOU LOOSE")
+                print("Max error number reached")
+                print(f"The word to guess was {solution}")
+                break
+        else :    
+            print("\n")
             print(" ==========Y= ")
-            print(" ||/       |  \n")
-        elif nb_error==2:
-            print(" ==========Y= ")
-            print(" ||/       |  ")
-            print(" ||        0  \n")
-        elif nb_error==3:
-            print(" ==========Y= ")
-            print(" ||/       |  ")
-            print(" ||        0  ")
-            print(" ||       /|\ \n")
-        elif nb_error==4:
-            print(" ==========Y= ")
-            print(" ||/       |  ")
-            print(" ||        0  ")
-            print(" ||       /|\ ")
-            print(" ||       / \  \n")
-        elif nb_error==5:  
-            print(" ==========Y= ")
-            print(" ||/       |  ")
-            print(" ||        0  ")
-            print(" ||       /|\ ")
-            print(" ||       / \  ")  
-            print(" ||            ")                 
-            print("/||\      (x)  ")
+            print(" ||/          ")
+            print(" ||     O U F ")
+            print(" ||           ")
+            print(" ||        0  ")  
+            print(" ||       /|\  ")                 
+            print("/||\      / \  ")
             print("==============\n")
-            print("YOU LOOSE")
-            print("Max error number reached")
-            break
-    else :    
-        print("\n")
-        print(" ==========Y= ")
-        print(" ||/          ")
-        print(" ||     O U F ")
-        print(" ||           ")
-        print(" ||        0  ")  
-        print(" ||       /|\  ")                 
-        print("/||\      / \  ")
-        print("==============\n")
-        print("  - YOU WIN -      ")
-    break
+            print("  - YOU WIN -      ")
+        break
 
+initGame()
